d3ccb328c7d890bffc7805872571f9010a5e5bad
nathan <maerna1@gmail.com>
2022-05-24 18:45:57 +0200

    lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnimalsModule } from './Animals/animal.module';
-import { APP_FILTER } from "@nestjs/core";
-import { AnimalExceptionFilter } from "./Animals/exposition/filters/animal-exception.filter";
+import { APP_FILTER } from '@nestjs/core';
+import { AnimalExceptionFilter } from './Animals/exposition/filters/animal-exception.filter';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    AnimalsModule
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AnimalsModule],
   controllers: [AppController],
-  providers: [AppService,
+  providers: [
+    AppService,
     {
       provide: APP_FILTER,
       useClass: AnimalExceptionFilter,
-    }],
+    },
+  ],
 })
 export class AppModule {}
