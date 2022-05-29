eaeb2803687bb7f49cdf20a204afefbf500a4766
nathan <maerna1@gmail.com>
2022-05-29 19:59:42 +0200

    refactor name of adapter

--- src/recipients/adaptaters/recipient.adapter.ts
+++ src/recipients/adaptaters/recipient.adapter.ts
@@ -2,7 +2,7 @@ import { AddRecipientDto } from '../dto/add-recipient';
 import { RecipientResponse } from '../domain/recipient.response';
 
 export class RecipientAdapter {
-  public static fromDto(dto: AddRecipientDto) {
+  public static fromAnimalToAnimalResponse(dto: AddRecipientDto) {
     return new RecipientResponse(
       dto.name,
       dto.surname,
