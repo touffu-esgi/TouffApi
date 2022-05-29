113027c0db4331fee082429a25b88312e94e16ae
nathan <maerna1@gmail.com>
2022-05-29 12:50:41 +0200

    replace access to enum in AnimalTypeFactory

--- src/Animals/application/animals.service.ts
+++ src/Animals/application/animals.service.ts
@@ -1,21 +1,17 @@
-import { HttpException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AddAnimalDto } from '../dto/add-animal.dto';
 import { Animal } from '../domain/animal';
 import { AnimalRepositoryInMemory } from '../persistence/animal.repository.in-memory';
-import { AnimalTypes } from '../domain/animal.types';
-import { AnimalTypeNotAllowedException } from './exceptions/animal-type-not-allowed.exception';
+import { AnimalTypeFactory } from './exceptions/animal-type.factory';
 
 @Injectable()
 export class AnimalsService {
   constructor(private animalRepository: AnimalRepositoryInMemory) {}
 
   async add(dto: AddAnimalDto): Promise<void> {
-    if (AnimalTypes[dto.type] == undefined)
-      throw new AnimalTypeNotAllowedException(
-        `animal type : '${dto.type}' is not allowed`,
-      );
+    const animalType = AnimalTypeFactory.fromString(dto.type);
 
-    const animal = new Animal(dto.name, AnimalTypes[dto.type]);
+    const animal = new Animal(dto.name, animalType);
     this.animalRepository.save(animal);
   }
 
