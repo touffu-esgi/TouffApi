544176eb8535fea6847b22d4f2fef9d689a25d4e
Sarah <sschlegel@myges.fr>
2022-06-12 19:22:06 +0200

    hotfix add provider props in db

--- src/Providers/persistence/provider.repository.in-memory.ts
+++ src/Providers/persistence/provider.repository.in-memory.ts
@@ -25,6 +25,8 @@ export class ProviderRepositoryInMemory implements ProviderRepository {
       address: '2',
       radius: 3,
       base_tariff: 30.2,
+      profile_title: 'Touriste de service',
+      profile_desc: 'Bla-bla-bla',
     }),
   ];
 
