2c8c8122a2a8fca9ce906dbfb2c493eb25bf5615
Sarah <sschlegel@myges.fr>
2022-06-21 11:19:49 +0200

    correct availabilities when occupied

--- src/Availability/persistence/availability.repository.in-memory.ts
+++ src/Availability/persistence/availability.repository.in-memory.ts
@@ -53,6 +53,7 @@ export class AvailabilityRepositoryInMemory implements AvailabilityRepository {
     const availabilities: Availability[] = this.availabilities.filter(
       (availability) => availability.providerId === providerId,
     );
+    console.log(availabilities[0].dailyAvailability);
     if (availabilities.length > 0) return availabilities;
     throw new NotAvailableException(
       `Provider ${providerId} not available at the moment`,
