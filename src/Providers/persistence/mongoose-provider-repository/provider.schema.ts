c2fdccea821710e6691d7384bdf9630dffd66c22
nathan <maerna1@gmail.com>
2022-06-24 15:40:51 +0200

    provider schema done

---
+++ src/Providers/persistence/mongoose-provider-repository/provider.schema.ts
@@ -0,0 +1,42 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { ProviderProps } from '../../domain/provider.props';
+
+@Schema()
+export class ProviderSchemaProps {
+  @Prop()
+  name: string;
+
+  @Prop()
+  surname: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  phone?: string;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  base_tariff: number;
+
+  @Prop()
+  radius: number;
+
+  @Prop()
+  profile_title: string;
+
+  @Prop()
+  profile_desc: string;
+
+  @Prop()
+  id?: string;
+}
+
+export type ProviderDocument = ProviderProps & Document;
+export const ProviderSchema = SchemaFactory.createForClass(ProviderSchemaProps);
