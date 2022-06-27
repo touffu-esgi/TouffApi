860e6be6c4d63393b253e2958073a522ef8b80a9
Sarah <sschlegel@myges.fr>
2022-06-27 19:59:49 +0200

    user can't report oneself

--- src/Report/application/report.service.ts
+++ src/Report/application/report.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ReportRepositoryInMemory } from '../persistence/report.repository.in-memory';
 import { Report } from '../domain/report';
 import { AddReportDto } from '../dto/add-report.dto';
+import { CantReportOneselfException } from './exceptions/cant-report-oneself.exception';
 
 @Injectable()
 export class ReportService {
@@ -10,10 +11,12 @@ export class ReportService {
   async addReport(dto: AddReportDto): Promise<Report> {
     const newId = this.reportRepository.getNextId();
     const comment = dto.comment ? dto.comment : '';
+    if (dto.reportedUserId === dto.reportingUserId)
+      throw new CantReportOneselfException('You can not report yourself');
     const report = new Report(
       newId,
       dto.reportedUserId,
-      dto.reporterUserId,
+      dto.reportingUserId,
       comment,
     );
     return await this.reportRepository.add(report);
