49bf015017749a0ba5cfbc6d1211c87d72453bfa
Nathan-dev-dot <72788855+Nathan-dev-dot@users.noreply.github.com>
2022-07-01 09:58:04 +0200

    Delete mongoose.config.ts

--- src/mongoose.config.ts
+++
@@ -1,9 +0,0 @@
-import { MongooseModuleOptions } from '@nestjs/mongoose';
-
-const mongooseConfig: MongooseModuleOptions = {
-  useFactory: () => ({
-    uri: 'mongodb+srv://Touffu:AMGlXSwUGe4tdusb@touffu.qii9p.mongodb.net/Touffu?retryWrites=true&w=majority',
-  }),
-};
-
-export = mongooseConfig;
