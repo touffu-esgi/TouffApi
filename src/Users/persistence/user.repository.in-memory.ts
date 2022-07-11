e7ede790610ca9acd016a85210c096d6f6217651
nathan <maerna1@gmail.com>
2022-07-11 14:56:07 +0200

    refacto bill adapter

--- src/Users/persistence/user.repository.in-memory.ts
+++ src/Users/persistence/user.repository.in-memory.ts
@@ -6,9 +6,11 @@ import { UserUpdate } from '../domain/user.update';
 
 export class UserRepositoryInMemory implements UserRepository {
   usersMockRepositoryImplement: User[] = [
-    new User('1', 'nathan@nathan.nathan', 'password', '1', 'recipient'),
+    new User('1', 'nathan@nathan.nathan', 'password', '1', 'provider'),
     new User('2', 'sarah@sarah.sarah', 'password', '2', 'provider'),
     new User('3', 'Theo@Theo.Theo', 'password', '3', 'provider'),
+    new User('5', 'nathan@nathan.fr', '123456789', '1', 'recipient'),
+    new User('6', 'lucille@moineau.fr', '123456789', '2', 'provider'),
   ];
 
   async add(user: User): Promise<User> {
